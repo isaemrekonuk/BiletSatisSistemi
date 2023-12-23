c8f972bab4dbb68fca4718da15f88760d289c2cf
İsa Emre Konuk <isaemrekonuk9@gmail.com>
2023-12-23 20:11:02 +0300

    Genel güncelleme. Projede kullanılacak olan template entegre edilmeye başlandı.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using EmreBilet.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EmreBilet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/hakkimizda")]
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        [Route("/iletisim")]
+        public IActionResult Iletisim()
+        {
+            return View();
+        }
+
+        [Route("/ucuslar")]
+        public IActionResult Ucuslar()
+        {
+            return View();
+        }
+
+        [Route("/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

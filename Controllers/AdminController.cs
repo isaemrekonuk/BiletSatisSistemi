180e5a3a34e7b4376ccdd11dbb69cdb7ffd9b2d6
İsa Emre Konuk <isaemrekonuk9@gmail.com>
2023-12-30 16:58:51 +0300

    Admin paneli projeye entegre edildi

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using EmreBilet.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EmreBilet.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public AdminController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("/admin/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

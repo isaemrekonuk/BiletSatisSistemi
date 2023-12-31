88dba5592cbda6a00313a20f224f2e480f829fe1
İsa Emre Konuk <isaemrekonuk9@gmail.com>
2023-12-31 15:48:44 +0300

    Son düzenlemeler yapıldı.

--- Program.cs
+++ Program.cs
@@ -1,35 +1,6 @@
-using System.Globalization;
-using System.Reflection;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
-using WebApplication3.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-#region Localizer
-builder.Services.AddSingleton<LanguageService>();
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-builder.Services.AddMvc().AddViewLocalization().AddDataAnnotationsLocalization(options =>
-    options.DataAnnotationLocalizerProvider = (type, factory) =>
-    {
-        var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName);
-        return factory.Create(nameof(SharedResource), assemblyName.Name);
-    });
-
-builder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    var supportCultures = new List<CultureInfo>
-    {
-        new CultureInfo("en-US"),
-        new CultureInfo("de-DE"),
-        new CultureInfo("tr-TR"),
-    };
-    options.DefaultRequestCulture = new RequestCulture(culture: "tr-TR", uiCulture: "tr-TR");
-    options.SupportedCultures = supportCultures;
-    options.SupportedUICultures = supportCultures;
-    options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
-});
-#endregion
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -51,7 +22,6 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 app.UseRouting();
 
 app.UseAuthorization();
